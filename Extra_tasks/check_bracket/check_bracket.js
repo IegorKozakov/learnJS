9fc1f4268e1725fc46a7f5b3c4c9c21eb93dce71
Iegor Kozakov <kozakov.e@gmail.com>
2016-07-12 11:37:00 +0300

    Update Extra tasks

--- Extra_tasks/check_bracket/check_bracket.js
+++ Extra_tasks/check_bracket/check_bracket.js
@@ -1,35 +1,56 @@
 "use strict";
 
-module.exports = (bracket) => {
-    // Если в bracket больше 100000 символов возвращаем INVALID.
-    if(bracket.length > 100000){
-        return 'INVALID';
+module.exports = {
+    v1: (bracket) => {
+        // Если в bracket больше 100000 символов возвращаем INVALID.
+        if(bracket.length > 100000){
+            return 'INVALID';
+        }
+        // Среобразуем с массив
+        var bracket = bracket.split('');
+
+        // Если первый элемент массива - ) возвращаем INVALID.
+        if (bracket[0] === ')') {
+            return 'INVALID';
+        }
+
+        // Убираем все лишние значения, оставляя только скобки.
+        bracket.filter((item)=>{
+            return item === '('? true : item === ')' ? true : false;
+        })
+
+        // Если длинна массива не четная, то вернется INVALID.
+        if (bracket.length%2 !== 0) {
+            return 'INVALID';
+        }
+
+        // сортируем массив все открывающиеся скобки сначала массива.
+        bracket.sort();
+
+        // так как массив отсортирован, проверяем положение первой закрывающейся скобки.
+        // если она не равна половине массива. возвращаем INVALID.
+        if(bracket.length / bracket.indexOf(')') !== 2 ){
+            return 'INVALID';
+        }
+        return 'VALID';
+    },
+    v2: (bracket) => {
+
+        if(bracket.length > 100000 || bracket.charAt(0) === ')'){
+            return 'INVALID';
+        }
+
+        var a = null,
+            b = null;
+
+        for(let i = 0; i<bracket.length; i++){
+            if(bracket.charAt(i) == '(') {
+                a++;
+            } else if (bracket.charAt(i) == ')'){
+                b++;
+            }
+        }
+
+        return a === b ? 'VALID' : 'INVALID';
     }
-    // Среобразуем с массив
-    var bracket = bracket.split('');
-
-    // Если первый элемент массива - ) возвращаем INVALID.
-    if (bracket[0] === ')') {
-        return 'INVALID';
-    }
-
-    // Убираем все лишние значения, оставляя только скобки.
-    bracket.filter((item)=>{
-        return item === '('? true : item === ')' ? true : false;
-    })
-
-    // Если длинна массива не четная, то вернется INVALID.
-    if (bracket.length%2 !== 0) {
-        return 'INVALID';
-    }
-
-    // сортируем массив все открывающиеся скобки сначала массива.
-    bracket.sort();
-
-    // так как массив отсортирован, проверяем положение первой закрывающейся скобки.
-    // если она не равна половине массива. возвращаем INVALID.
-    if(bracket.length / bracket.indexOf(')') !== 2 ){
-        return 'INVALID';
-    }
-    return 'VALID';
 };
--- Extra_tasks/check_bracket/index.js
+++ Extra_tasks/check_bracket/index.js
@@ -1,7 +1,13 @@
 "use strict";
 
 const check_bracket = require('./check_bracket');
-console.log(check_bracket('()(())(())')); // true
-console.log(check_bracket('()(())(())(')); // false
-console.log(check_bracket('()(())(())((')); // false
-console.log(check_bracket(')(')); // false
+console.log('version 1');
+console.log(check_bracket.v1('()(())(())')); // VALID
+console.log(check_bracket.v1('()(())(())(')); // INVALID
+console.log(check_bracket.v1('()(())(())((')); // INVALID
+console.log(check_bracket.v1(')(')); // INVALID
+console.log('version 2');
+console.log(check_bracket.v2('()(())(())')); // VALID
+console.log(check_bracket.v2('()(())(())(')); // INVALID
+console.log(check_bracket.v2('()(())(())((')); // INVALID
+console.log(check_bracket.v2(')(')); // INVALID
